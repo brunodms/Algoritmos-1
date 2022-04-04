ea956a86fa61fa4b995851289c2973624cfea431
Bruno de Macedo Sanagiotto <62435631+brunodms@users.noreply.github.com>
2022-04-04 17:27:09 -0300

    atualizado

--- SistemaSuperCC-bruno.py
+++ SistemaSuperCC-bruno.py
@@ -1,71 +1,98 @@
-cod = int
-class Prod:
-    prod_cod = None
-    prod_name = None
-    prod_price = None
-    prod_stock = None
-produto = []
-compras = []
-
-fn = int(input("Digite 1 para Cadastro de novos produtos, 2 para Atualizaçoes, 3 para Vendas, 4 para Consultas e 5 para Relatorio"))
-if fn == 1:
-    while True:
-        p = Prod()
-        p.prod_cod = int(input("\nCódigo(0 encerra o codigo): "))
-        if p.prod_cod == 0:
-            break
-        elif p.prod_cod in produto.prod_cod:
-            print("Codigo já cadastrado")
-            continue
-        p.prod_name = input("Nome: ")
-        p.prod_price = float(input("Preço: "))
-        p.prod_stock = int(input("Estoque: "))
-        produto.append(p)
-        
-if fn == 2:
-    cod = input("Digite o Código do Produto: ")
-    for i in range(len(produto)):
-        if cod in produto[i].prod_cod:
-            att = int(input("Digite 1 para atualzar o preço, 2 para atualizar o estoque ou 3 para atualizar ambos"))
-            if att == 1:
-                produto[i].prod_price = input("Novo Preço: ")
-            elif att == 2:
-                produto[i].prod_stock = input("Novo Estoque: ")
-            elif att == 3:
-                produto[i].prod_price = input("Novo Preço: ")
-                produto[i].prod_stock = input("Novo Estoque: ")
+# Criação da Classe do Produto
+class tProd:
+    codigo = 0
+    name = None
+    price = 0.0
+    stock = 0
+# Definição da Classe do Produto
+def cadProd():
+    prod = tProd()
+    prod.codigo = int(input("Código: "))
+    prod.name = input("Nome: ")
+    prod.price = float(input("Preço: "))
+    prod.stock = int(input("Estoque: "))
+    return prod
+# Listar Variaveis do Produto  
+def showProd(p):
+    print("%-8d %-20s %.2f %7d"%(p.codigo, p.name, p.price, p.stock))
+# Buscar Variaveis do Produto Pelo Codigo       
+def searchProd(pr,c):
+    for i in range(len(pr)):
+        if pr[i].codigo==c:
+            return i
+        return -1
+# Atualizar Variaveis do Produto(Somente Preço e Estoque)
+def updateProd(l,codigo,prc,stk):
+    pos = searchProd(l,codigo)
+    if pos != -1:
+        l[pos].price=prc
+        l[pos].stock=stk
+# Registrar Compras
+def comprasProd(lproduto, cods, compra):
+    for i in range(len(lproduto)):
+        if cods in lproduto[i].codigo:
+            print(lproduto[i].name)
+            compra = int(input("informe a quantidade desejada: "))
+            if compra > lproduto[i].stock:
+                print(f"Estoque insuficiente, a quantidade disponivel é: ", lproduto[i].stock)
             else:
-                print("O Valor informado é Invalido!")
-        else:
-            print("Código invalido")
-elif fn == 3:
-    cod = input("Digite o Código do Produto: ")
-    for i in range(len(produto)):
-        if cod in produto[i].prod_cod:
-            print(produto[i].prod_name)
-            compra = int(input("Informe a quantidade desejada: "))
-            if compra >= produto[i].prod_stock:
-                print("Estoque insuficiente, a quantidade disponivel é: ", produto[i].prod_stock)
-            else:
-                c = Prod()
-                c.compra_cod = produto[i].prod_cod
-                c.compra_name = produto[i].prod_name
+                c = tProd()
+                c.compra_cod = lpd[i].cod
+                c.compra_name = lpd[i].name
                 c.compra_qtd = compra
-                c.compra_price = compra * produto[i].prod_price
-                compras.append(c)
-                produto[i].prod_stock -= compra
+                c.compra_price = compra * lpd[i].price
+                lpd[i].stock -= compra
+                return c    
+# Listar Todas as Variaveis do produto Organizadamente
+def listProd(lprod):
+    print("Código   Nome                 Preço  Estoque")
+    print("============================================")
+    for i in range(len(lprod)):
+        showProd(lprod[i])          
+# Menu
+def menu():
+    print("1- Incluir")
+    print("2- Atualizar Produto")
+    print("3- Registrar Compra")
+    print("4- Cosultar Produto")
+    print("5- Relatorio de Produtos")
+    print('0- Sair')
+    fn=int(input())
+    return fn
+# Criação da Lista de Produtos
+lpd = []
+compras = []
+# Começo do Loop
+while True:
+    fn=menu()
+# Sair
+    if fn == 0:
+        break
+# Incluir
+    if fn == 1:
+        pr=cadProd()
+        lpd.append(pr)
+# Atualizar
+    if fn == 2:
+        cd = input("Digite o Código do Produto: ")
+        pc = float(input("Novo preço: "))
+        sk = int(input("Nova Quantidade: "))
+        updateProd(lpd, cd, pc, sk)
+# Registrar
+    if fn == 3:
+        cod = input("Digite o Código do Produto: ")
+        comprasProd(lpd, cod, compras)
+        
+# Consultar
+    if fn == 4:
+        c=int(input("Digite o Código do Produto: "))
+        pos=searchProd(lpd,c)
+        if pos == -1:
+            print("Produto não encontrado!")
         else:
-            print("Código invalido")
-            
-elif fn == 4:
-    cod = input("Digite o Código do Produto: ")
-    for i in range(len(produto)):
-        if cod in produto[i].prod_cod:
-            print(" Código: ", produto[i].prod_cod, " Nome: ", produto[i].prod_name, " Preço: ", produto[i].prod_price, " Estoque: ", produto[i].prod_stock)
-        else:
-            print("Código invalido")
-elif fn == 5:
-    for i in range(len(produto)):
-        print(" Código: ", produto[i].prod_cod, " Nome: ", produto[i].prod_name, " Preço: ", produto[i].prod_price, " Estoque: ", produto[i].prod_stock)
-else:
-    print("Função invalida")
+            print("Código   Nome                 Preço  Estoque")
+            print("============================================")
+            showProd(lpd[pos])
+# Listar todos
+    if fn == 5:
+        listProd(lpd)
